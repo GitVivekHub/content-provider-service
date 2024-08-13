7bf4f90acbb1ffc9a2a143ddf08b658bf510c99b
Suraj <suraj_k@tekditechnologies.com>
2024-08-13 19:15:20 +0530

    update contents

--- src/services/hasura/hasura.service.ts
+++ src/services/hasura/hasura.service.ts
@@ -346,15 +346,81 @@ export class HasuraService {
   }
 
   async createContent(id, createContentdto) {
-    const query = `mutation InsertFlnContent($user_id:Int,$description: String,$code:String,$competency:String,$contentType:String,$domain:String,$goal:String,$image:String,$language:String,$link:String,$sourceOrganisation:String,$themes:String,$title:String, $content_id: String, $publisher: String, $collection: Boolean, $urlType: String, $mimeType: String, $minAge: Int, $maxAge: Int, $author: String, $learningOutcomes: String, $category: String ) {
-      insert_fln_content(objects: {user_id:$user_id,description: $description,code: $code, competency:$competency, contentType:$contentType, domain:$domain, goal:$goal, image:$image, language:$language, link: $link, sourceOrganisation: $sourceOrganisation, themes: $themes, title: $title, content_id: $content_id, publisher: $publisher, collection: $collection, urlType: $urlType, mimeType: $mimeType, minAge: $minAge, maxAge: $maxAge, author: $author, learningOutcomes: $learningOutcomes, category: $category  }) {
+    // const query = `mutation InsertFlnContent($user_id:Int,$description: String,$code:String,$competency:String,$contentType:String,$domain:String,$goal:String,$image:String,$language:String,$link:String,$sourceOrganisation:String,$themes:String,$title:String, $content_id: String, $publisher: String, $collection: Boolean, $urlType: String, $mimeType: String, $minAge: Int, $maxAge: Int, $author: String, $learningOutcomes: String, $category: String ) {
+    //   insert_fln_content(objects: {user_id:$user_id,description: $description,code: $code, competency:$competency, contentType:$contentType, domain:$domain, goal:$goal, image:$image, language:$language, link: $link, sourceOrganisation: $sourceOrganisation, themes: $themes, title: $title, content_id: $content_id, publisher: $publisher, collection: $collection, urlType: $urlType, mimeType: $mimeType, minAge: $minAge, maxAge: $maxAge, author: $author, learningOutcomes: $learningOutcomes, category: $category  }) {
+    //     returning {
+    //       id
+    //       user_id
+    //     }
+    //   }
+    // }
+    // `
+    const query = `mutation MyMutation(
+      $user_id:Int, 
+      $branch: jsonb, 
+      $contentType: String,  
+      $crop: String, 
+      $description: String, 
+      $district: jsonb, 
+      $expiryDate: String, 
+      $fileType: String, 
+      $monthOrSeason: String, 
+      $publishDate: String, 
+      $publisher: String, 
+      $region: jsonb, 
+      $target_users: jsonb,  
+      $title: String, 
+      $state: jsonb, 
+      $url: String,
+      $icon: String
+      ){${this.nameSpace} {
+        insert_Content(objects: {
+        user_id:$user_id
+        branch: $branch
+        contentType: $contentType
+        crop: $crop
+        description: $description
+        district: $district
+        expiryDate: $expiryDate
+        fileType: $fileType
+        monthOrSeason: $monthOrSeason
+        publishDate: $publishDate
+        publisher: $publisher
+        region: $region
+        state: $state
+        target_users: $target_users
+        title: $title
+        url: $url
+        icon: $icon
+      }) {
+        affected_rows
         returning {
           id
+          branch
+          contentType
+          content_id
+          crop
+          description
+          district
+          expiryDate
+          fileType
+          icon
+          fileType
+          monthOrSeason
+          publishDate
+          publisher
+          region
+          state
+          target_users
+          title
+          url
           user_id
         }
       }
+      }
     }
     `
+
     try {
       console.log("Response ", createContentdto);
       const response = await this.queryDb(query, { user_id: id, ...createContentdto });
@@ -537,69 +603,12 @@ export class HasuraService {
   }
 
   async editContent(id, createContentdto) {
-    // console.log("createContentdto", createContentdto)
-    // const query = `mutation UpdateMyData($id: Int!, $themes: String, $code: String, $competency: String, $contentType: String, $description: String, $domain: String, $goal: String, $language: String, $link: String, $sourceOrganisation: String, $title: String, $image: String, $publisher: String, $collection: Boolean, $urlType: String, $url: String, $mimeType: String, $minAge: Int, $maxAge: Int, $author: String, $curricularGoals: String, $learningOutcomes: String, $category: String) {
-    //   update_icar_content(where: { id: { _eq: $id } }, _set: {
-    //     themes: $themes
-    //     code: $code
-    //     competency: $competency
-    //     contentType: $contentType
-    //     description: $description
-    //     domain: $domain
-    //     goal: $goal
-    //     language: $language
-    //     link: $link
-    //     sourceOrganisation: $sourceOrganisation
-    //     title: $title
-    //     image: $image
-    //     publisher: $publisher
-    //     collection: $collection
-    //     urlType: $urlType
-    //     url: $url
-    //     mimeType: $mimeType
-    //     minAge: $minAge
-    //     maxAge: $maxAge
-    //     author: $author
-    //     curricularGoals: $curricularGoals
-    //     learningOutcomes: $learningOutcomes
-    //     category: $category
-    //   }) {
-    //     affected_rows
-    //     returning {
-    //       id
-    //       user_id
-    //       code
-    //       competency
-    //       contentType
-    //       description
-    //       domain
-    //       goal
-    //       image
-    //       language
-    //       link
-    //       sourceOrganisation
-    //       themes
-    //       title
-    //       image
-    //       publisher
-    //       collection
-    //       urlType
-    //       url
-    //       mimeType
-    //       minAge
-    //       maxAge
-    //       author
-    //       curricularGoals
-    //       learningOutcomes
-    //       category
-    //     }
-    //   }
-    // }
-    // `
+    console.log("id", id)
+     console.log("createContentdto", createContentdto)
 
     const query = `mutation UpdateMyData(
       $id: Int!, 
-      $branch: String, 
+      $branch: jsonb, 
       $contentType: String,  
       $crop: String, 
       $description: String, 
@@ -615,9 +624,8 @@ export class HasuraService {
       $state: jsonb, 
       $url: String,
       $icon: String
-      ) {
-      update_icar_content(where: { id: { _eq: $id } }, _set: {
-        
+      ){${this.nameSpace} {
+        update_Content(where: { id: { _eq: $id } }, _set: {
         branch: $branch
         contentType: $contentType
         crop: $crop
@@ -657,11 +665,13 @@ export class HasuraService {
           title
           url
           user_id
-          
         }
+      }
       }
     }
     `
+    console.log("query", query)
+    console.log("variables:", { id: id, ...createContentdto });
     try {
       const response = await this.queryDb(query, { id: id, ...createContentdto });
       console.log(response)
