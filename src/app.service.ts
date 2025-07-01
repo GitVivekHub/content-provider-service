8394016e5b81ff19c76da59006ccfd6d35bd52de
Yogini Tayade <yogini.tayade@tekditechnologies.com>
2025-07-01 16:17:52 +0530

    refactored response of otp

--- src/app.service.ts
+++ src/app.service.ts
@@ -1032,8 +1032,16 @@ export class AppService {
         let decryptedData: any = await decryptRequest(response.d.output, key);
         console.log("Response of VerifyOTP", response);
         console.log("Response from decryptedData(verifyOTP)", decryptedData);
-        response["status"] =
-          response.d.output.Rsponce != "False" ? "OK" : "NOT_OK";
+        
+        try {
+          const parsedDecryptedData = JSON.parse(decryptedData);
+          response.d.output = parsedDecryptedData;
+          response["status"] = parsedDecryptedData.Rsponce === "True" ? "OK" : "NOT_OK";
+        } catch (e) {
+          console.error("Error parsing decrypted data:", e);
+          response["status"] = "NOT_OK";
+        }
+        
         return response;
       } else {
         return {
@@ -1406,9 +1414,9 @@ export class AppService {
         };
    // Build status message
       let otpMessage = "Request for OTP is sent. Please enter the OTP when received and Submit.";
-      if (!isValidPhone) {
-        otpMessage += " However, the provided contact phone number is invalid and will not be used.";
-      }
+      // if (!isValidPhone) {
+      //   otpMessage += " However, the provided contact phone number is invalid and will not be used.";
+      // }
         return {
           context: { ...body.context, action: "on_init", timestamp: new Date().toISOString() },
           message: {
